cd0a276c18c44d2302e10b22d0334ca9da2aa137
Iasmincost134 <133365214+Iasmincost134@users.noreply.github.com>
2024-01-26 16:46:11 -0300

    Add files via upload

---
+++ exercicio1.sql
@@ -0,0 +1,23 @@
+/*create database biblioteca*/
+/*use biblioteca*/
+/*create table livros(
+	isbn int(13),
+    titulo varchar(30),
+    ano int(4),
+    editora varchar(30),
+    autores varchar(30)
+);*/
+/*create table autores(
+    nome varchar(30),
+    nacionalidade varchar(30)
+);*/
+/*create table categoria(
+    codigo int(4),
+    descricao varchar(30)
+);*/
+/*insert into livros(isbn, titulo, ano, editora, autores) values (125427, "livro infantil", 2000, "editora da iaas", "ias gordilho")*/
+/*select * from livros*/
+/*insert into autores(nome, nacionalidade) values ("ias gordilho", "brasileiro")
+/*select * from autores*/
+/*insert into categoria(codigo, descricao) values (1234, "livro muito bom iaas")*/
+/*select * from categoria*/
---
+++ exercicio2.sql
@@ -0,0 +1,25 @@
+/*create database firma*/
+/*use firma*/
+
+/*create table produto(
+    codigo int(4),
+    nome varchar(30),
+    categoria varchar(30),
+    preco varchar(3)
+);*/
+
+/*create table cliente(
+    codigo int(4),
+    nome varchar(30),
+    endereco varchar(30),
+    telefone int(8),
+	staus varchar(30)
+);*/
+
+/*create table pedido(
+    numero int(4),
+    data int(8)
+);*/
+
+/*insert into produto(codigo, nome, categoria, preco) values (1254, "bolda de futebol", "esporte", 10)*/
+select * from produto
